35e6ebad8eb7e936d7806483fd99e63a1f87c419
luozj <luozj@yunjiacloud.com>
2021-01-26 12:59:02 +0800

    读取数据源

--- BusiDataAnalysis.py
+++ BusiDataAnalysis.py
@@ -1,3 +1,6 @@
 import pandas as pd
+### 读取数据源
+data = pd.read_csv('E:/BaiduNetdiskDownload/最近数据分析15期/第七章 Python/第七章/20200831数据源/20200831数据源.csv',encoding='ISO-8859-1')
+print(data)
 
-pd.read_csv('')
+### 清洗数据
