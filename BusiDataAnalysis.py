44495b082b64e6862b5e9a3673403fa4274d2ed1
lois_daluo <1494833094@qq.com>
2021-02-24 20:52:51 +0800

    Revert "清洗RowID"
    This reverts commit 6d3311c9

--- BusiDataAnalysis.py
+++ BusiDataAnalysis.py
@@ -5,7 +5,7 @@ data = pd.read_csv('E:/BaiduNetdiskDownload/最近数据分析15期/第七章 Py
 # print(data)
 # 清洗数据
 #1.不合理的数据
-## 1.1跟据业务需要提取数据，发货日期早于下单日期  不合理值
+## 1.1跟据业务需要提取数据，发货日期早于下单日期
 data['OrderDate'] = pd.to_datetime(data['OrderDate'])
 data['ShipDate'] = pd.to_datetime(data['ShipDate'])
 # 时间差
@@ -18,43 +18,16 @@ data.drop(index = data[data.interval < 0].index,inplace=True)
 # 没有售价为负的数据
 # print(data[data.Sales < 0])
 ## 1.3 查看数据
-# print(data.info())
-# print(data.isna().sum())
+print(data.info())
+print(data.isna().sum())
 ## (1)清洗PostalCode列 40000+为空
 ## (2)清洗ShipMode列 11为空
 # print(data.describe()) ## 可以查看某一列的数据情况 最大最小值、均值、众数
 # print(data.head())
 # print(data.count())
-## 1.4 清洗RowID 重复值
-
-###  1111
-####  333
-
-# print(data['RowID'])
-# print(data.RowID.size)
-# print(data.RowID.unique().size)
-# print(data[data.RowID.duplicated()])
-data.drop(index=data[data.RowID.duplicated()].index,inplace=True)
-# print(data.RowID.size)
-
+## 1.4 清洗RowID
 ## 1.5 清洗ShipMode
-# print(data.ShipMode.size)
-# print(data[data.ShipMode.isnull()])
-# 众数填充空值
-# print(data.ShipMode.mode()[0])
-data['ShipMode'].fillna(value=data.ShipMode.mode()[0],inplace=True)
-# print(data[data.ShipMode.isnull()])
-## 1.6 清洗Discount 折扣 用平均值填充
-# print(data[data.Discount > 1]) ## 有数据
-# print(data[data.Discount < 0]) ## 无数据
-print(data[data['Discount'] > 1])
-
-data['Discount']=data.Discount.mask(data.Discount > 1,None)
-# print(data[data['Discount'].isna()])
-# 平均折扣
-discount_mean = data[data.Discount.notnull()].Discount.mean()
-print(discount_mean)
-data.fillna(value=data.Discount,)
+## 1.6 清洗Discount
 ## 1.7 清洗PostalCode列
 
 
