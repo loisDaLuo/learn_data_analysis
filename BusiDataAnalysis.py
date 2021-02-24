9a4812c36f622e3b42e3cf2adceb7cadd52d8a2b
lois_daluo <1494833094@qq.com>
2021-02-24 21:30:51 +0800

    Revert "读取数据源"
    This reverts commit 35e6ebad8eb7e936d7806483fd99e63a1f87c419.

--- BusiDataAnalysis.py
+++ BusiDataAnalysis.py
@@ -1,6 +1,3 @@
 import pandas as pd
-### 读取数据源
-data = pd.read_csv('E:/BaiduNetdiskDownload/最近数据分析15期/第七章 Python/第七章/20200831数据源/20200831数据源.csv',encoding='ISO-8859-1')
-print(data)
 
 pd.read_csv('')
